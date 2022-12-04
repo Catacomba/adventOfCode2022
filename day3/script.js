40f44d162610d7bdf893aed558cc94e4ea92507c
Kevin Šuc <kevinsuc16@gmail.com>
2022-12-04 22:49:27 +0100

    Partially implemented day3

--- day3/script.js
+++ day3/script.js
@@ -11,9 +11,78 @@ const fs = require('fs');
 var path = require('path');
 
 const dataPath = path.join(__dirname, '..', 'day3', 'data.txt');
-const data = fs.readFileSync(dataPath, 'UTF-8')
+const data = fs.readFileSync(dataPath, 'UTF-8').split(/\r?\n/)
+
+const priorities = [
+    '-',
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z',
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z'
+]
 
 console.time("Timer");
+
+for (let i = 0; i < data.length; i++) {
+    const half = data[i].length / 2;
+    let firstCompartment = data[i].substring(0, half);
+    let secondCompartment = data[i].substring(half, data.length - 1)
+        // console.log(`
+        // Together: ${data[i]}
+        // First:    ${firstCompartment}
+        // Second:   ${secondCompartment}
+        // Together2:${firstCompartment}${secondCompartment}
+        // `);
+    let
+}
 //2d array
 // var charCount = [
 //     [][]
