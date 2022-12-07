1b72518cffd31948b3f008c6c85d077d53103dcf
Kevin <kevinsuc16@gmail.com>
2022-12-07 20:03:34 +0100

    Finished day 4 part 2

---
+++ day4/part2.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+var path = require('path');
+
+//Relative path to data.txt
+const dataPath = path.join(__dirname, '..', 'day4', 'data.txt');
+const pairsOfElves = fs.readFileSync(dataPath, 'UTF-8').split(/\r?\n/)
+
+var uselessElves = 0;
+console.time('timer');
+for(let i = 0; i < pairsOfElves.length; i++){
+    const pair = pairsOfElves[i].split(',');
+    const firstElfArea = pair[0].split('-');
+    const secondElfArea = pair[1].split('-');
+    const firstElfStart = Number(firstElfArea[0]);
+    const firstElfEnd = Number(firstElfArea[1]);
+    const secondElfStart = Number(secondElfArea[0]);
+    const secondElfEnd = Number(secondElfArea[1]);
+
+    //Instead of checking all overlaps, check not overlaps
+    if(firstElfEnd < secondElfStart){
+        continue;
+    }
+    if(secondElfEnd < firstElfStart){
+        continue;
+    }
+    uselessElves++;
+}
+console.timeEnd('timer');
+console.log(uselessElves);
